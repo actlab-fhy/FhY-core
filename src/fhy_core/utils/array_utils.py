d1bcae6f100a02baa1a6f2cdd9e5c3dbad736bb8
Christopher Priebe <chrisp9090@gmail.com>
2024-11-15 15:21:48 -0800

    fix(utils.array_utils): Updated implementation for get_array_size_in_bits.

--- src/fhy_core/utils/array_utils.py
+++ src/fhy_core/utils/array_utils.py
@@ -3,11 +3,9 @@
 __all__ = ["get_array_size_in_bits"]
 
 from collections.abc import Sequence
+from math import prod
 
 
 def get_array_size_in_bits(shape: Sequence[int], element_size_in_bits: int) -> int:
     """Return the size of an array in bits."""
-    size = 1
-    for dim in shape:
-        size *= dim
-    return size * element_size_in_bits
+    return prod(shape) * element_size_in_bits
