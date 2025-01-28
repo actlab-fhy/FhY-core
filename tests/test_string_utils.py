cdb49bd68cf7ee5098a80064425320471629f81d
Christopher Priebe <chrisp9090@gmail.com>
2025-01-27 17:28:22 -0800

    chore(test_string_utils): Refactored to parametrize test rather than have individual tests.

--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -1,44 +1,20 @@
 """Tests the string utilities module."""
 
+import pytest
 from fhy_core.utils.str_utils import format_comma_separated_list
 
 
-def test_separate_list_of_numbers_with_comma():
-    """Test separating a list of integers with commas and spaces."""
-    items = [1, 2, 3]
-    expected = "1, 2, 3"
-    result = format_comma_separated_list(items)
-    assert result == expected
-
-
-def test_separate_list_of_numbers_with_comma_without_space():
-    """Test separating a list of integers with commas and no spaces."""
-    items = [1, 2, 3]
-    expected = "1,2,3"
-    result = format_comma_separated_list(items, add_space=False)
-    assert result == expected
-
-
-def test_separate_empty_list_with_comma():
-    """Test separating an empty list with commas and spaces."""
-    items = []
-    expected = ""
-    result = format_comma_separated_list(items)
-    assert result == expected
-
-
-def test_separate_single_item_list_with_comma():
-    """Test separating a single-item list with commas and spaces."""
-    items = [1]
-    expected = "1"
-    result = format_comma_separated_list(items)
-    assert result == expected
-
-
-def test_separate_list_of_numbers_with_comma_with_custom_str_func():
-    items = [1, 2, 3]
-    expected = "1 -,2 -,3 -"
-    result = format_comma_separated_list(
-        items, str_func=lambda x: f"{x} -", add_space=False
-    )
+@pytest.mark.parametrize(
+    "items, add_space, str_func, expected",
+    [
+        ([1, 2, 3], True, str, "1, 2, 3"),
+        ([1, 2, 3], False, str, "1,2,3"),
+        ([], True, str, ""),
+        ([1], True, str, "1"),
+        ([1, 2, 3], False, lambda x: f"{x} -", "1 -,2 -,3 -"),
+    ],
+)
+def test_format_comma_separated_list(items, add_space, str_func, expected):
+    """Test various cases for format_comma_separated_list."""
+    result = format_comma_separated_list(items, add_space=add_space, str_func=str_func)
     assert result == expected
